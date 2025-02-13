6ba15728cc5a482f4fc2cb3b894739dce9edd9fb
Makoyy07 <>
2025-02-13 17:06:43 +0800

    Last Sync: 2025-02-13 09:06:43 (Mobile)

--- python problem_01.py
+++ python problem_01.py
@@ -1,13 +1,13 @@
-# Mc James Jardiolin
+# YOUR NAME
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
     print("Square the Number Program")
-    
-    number = int(input("Enter a number: "))
-    print(f"The square of {number} is {(number * number):.2f}")
 
+      number = int(input("Enter a number: "))
+      print(f"The square of {number} is {(number * number):.2f}")
+      
 if __name__ == "__main__":
     main()
