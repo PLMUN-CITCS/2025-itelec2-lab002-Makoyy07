a263f8c10c6d7b616a79384234d18ef0481ae636
Makoyy07 <>
2025-02-13 17:15:54 +0800

    Last Sync: 2025-02-13 09:15:54 (Mobile)

--- python problem_01.py
+++ python problem_01.py
@@ -4,10 +4,16 @@
 # Simple Calculator Program
 
 def main():
-    print("Square the Number Program")
+    print("Simple Calculator Program")
 
-      number = int(input("Enter a number: "))
-      print(f"The square of {number} is {(number * number):.2f}")
+      num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print(f"The sum is {num1 + num2}")
+print(f"The difference is {num1 - num2}")
+print(f"The product is {num1 * num2}")
+print(f"The quotient is {(num1 / num2):.2f}")
+
       
 if __name__ == "__main__":
     main()
