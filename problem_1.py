68e7a31f7a986e39fb45d2d44b67ef00c26cda84
Makoyy07 <>
2025-02-13 17:48:22 +0800

    Last Sync: 2025-02-13 09:48:22 (Mobile)

---
+++ problem_1.py
@@ -0,0 +1,15 @@
+# Mc James Jardiolin
+# ITELEC2
+# Problem Set 01 - Problem 02
+# Square the Number Program
+
+def main():
+    print("Square the Number Program")
+
+    number = int(input("Enter a number: "))
+
+    # Print the square with two decimal places
+    print(f"The square of {number} is {number * number:.2f}")
+
+if __name__ == "__main__":
+    main()
